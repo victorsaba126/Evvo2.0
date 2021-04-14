c4aaaf4816873d634c20e9711379e8688de9bea5
eduardotorressubias <edu.torres9@hotmail.com>
2021-04-14 11:45:34 +0200

    caidaPuente

---
+++ Assets/scripts/TriggerAltura.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAltura : MonoBehaviour
+{
+    public GameObject altura;
+    private float counterTime;
+    private float coldown = 2f;
+    private bool coldown2 = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(coldown2 == true)
+        {
+            counterTime += Time.deltaTime;
+            if (counterTime >= coldown)
+            {
+                altura.SetActive(false);
+                counterTime = 0;
+                coldown2 = false;
+            }
+        }
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            altura.SetActive(true);
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            coldown2 = true;
+            
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            altura.SetActive(true);
+
+        }
+    }
+
+}
