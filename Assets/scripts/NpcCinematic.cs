6a8ec46e9c0504514f007d7bfab603076f6dd208
eduardotorressubias <edu.torres9@hotmail.com>
2021-04-19 15:05:00 +0200

    cinematic_ok

--- Assets/scripts/NpcCinematic.cs
+++ Assets/scripts/NpcCinematic.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class NpcCinematic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public LayerMask whatIsPlayer;
+    public float range;
+    private bool isPlayerHere;
+    public GameObject cinematic;
+    public GameObject freelook;
+
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       isPlayerHere=  Physics.CheckSphere(transform.position, range, whatIsPlayer);
+
+        if (isPlayerHere)
+        {
+            freelook.SetActive(false);
+            cinematic.SetActive(true);
+        }
+        else
+        {
+            freelook.SetActive(true);
+            cinematic.SetActive(false);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, range);
+       
+
     }
 }
