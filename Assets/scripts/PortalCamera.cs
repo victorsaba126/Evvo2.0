fc7b84b15475c622cc698dedb199839bdb760b11
eduardotorressubias <edu.torres9@hotmail.com>
2021-04-20 12:10:58 +0200

    portal_en_pruebas

---
+++ Assets/scripts/PortalCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        
+
+    }
+}
