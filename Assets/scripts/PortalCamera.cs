8de2d0f54010272975600868a3e2254c04ec062e
eduardotorressubias <edu.torres9@hotmail.com>
2021-04-21 00:32:21 +0200

    comit_prueba

--- Assets/scripts/PortalCamera.cs
+++ Assets/scripts/PortalCamera.cs
@@ -21,6 +21,11 @@ public class PortalCamera : MonoBehaviour
         Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
         transform.position = portal.position + playerOffsetFromPortal;
 
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
         
 
     }
