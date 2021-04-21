580790728726c1d6d02872b00bed26d46cbc3f1d
eduardotorressubias <edu.torres9@hotmail.com>
2021-04-21 20:14:17 +0200

    commit_timeofday

--- Assets/scripts/TimeOfDay.cs
+++ Assets/scripts/TimeOfDay.cs
@@ -14,6 +14,7 @@ public class TimeOfDay : MonoBehaviour
     public float transitionTime;
     public bool nightOn = false;
     private float numero = 0;
+    private float segundos = 0;
 
    
 
@@ -22,9 +23,20 @@ public class TimeOfDay : MonoBehaviour
     {
         counterTime += Time.deltaTime;
         numero += 0.02f;
+
+        segundos = segundos + 1;
+        //if (segundos == 50)
+        //{
+        //    Debug.Log("numero " + numero);
+        //    segundos = 0;
+        //}
+
         //Debug.Log("numero "+numero);
         //Debug.Log("countertime "+counterTime);
-        
+        //if (numero/2 == 0)
+        //{
+        //    Debug.Log("numero " + numero);
+        //}
         
         
        if(counterTime>= transitionTime)
@@ -43,11 +55,24 @@ public class TimeOfDay : MonoBehaviour
             }
             if (nightOn)
             {
-                night.weight = numero / transitionTime; /*(counterTime * -0.00001f);*/
+                if (segundos == 50)
+                {
+                    night.weight = numero / transitionTime; /*(counterTime * -0.00001f);*/
+                    Debug.Log("numero " + numero);
+                    segundos = 0;
+                }
+                
             }
             else
             {
-                night.weight = numero/ transitionTime;/*(counterTime * 0.00001f);*/
+                if (segundos == 50)
+                {
+                    Debug.Log("numero " + numero);
+                    night.weight = numero / transitionTime;/*(counterTime * 0.00001f);*/
+                    segundos = 0;
+                }
+                
+
             }
 
             //day.weight = day.weight+(counterTime * - 0.01f);
